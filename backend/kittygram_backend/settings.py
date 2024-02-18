08f46ea2885a6f832610deae49c1da74f05aebaa
Artem Kulikov <chigar2010@yandex.ru>
2024-02-18 17:11:17 +0200

    fix after last review

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'my_secret_key')
 
-DEBUG = os.getenv('DEBUG', False)
+DEBUG = os.getenv('DEBUG') == 'True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
 
